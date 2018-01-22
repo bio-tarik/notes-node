c84c2a5c5b3a7ab2f2c024dd1afc6e41d8c0a527
bio-tarik <bio.tarik@gmail.com>
2018-01-21 23:19:31 -0200

    Using command line arguments to receive parameters into the program

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ const _ = require('lodash');
 const notes = require('./notes.js');
 
 console.log('###################');
+
+var command = process.argv[2];
+
+console.log("Command: ", command);
+
+if (command === 'add') {
+    console.log('Adding new note');
+}
+else if (command === 'list') {
+    console.log('Listing all notes');
+}
+else if (command === 'read') {
+    console.log('Reading note');
+}
+else if (command === 'delete') {
+    console.log('Deleting note');
+}
+else {
+    console.log('Command not recognized');
+}
